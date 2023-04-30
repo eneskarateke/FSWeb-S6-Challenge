9b775eb85099ee59f92a5354da72734edc841230
eneskarateke <eneskarateke@gmail.com>
2023-04-30 18:18:59 +0300

    handler'daki 6 tane karakter dom'a yansıtıldı bazı özellikleriyle birlikte, filmler datası henüz çekilmedi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Karakter from "./components/Karakter";
+
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [karakterler, setKarakterler] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://swapi.dev/api/people/");
+      console.log("result -->",result.data)
+      setKarakterler(result.data);
+    };
+
+    fetchData();
+  }, []);
+  
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      
+      <Karakter data={karakterler}/>
+    
     </div>
   );
 }
+
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,79 @@
 // Karakter bileşeniniz buraya gelecek
+import React from 'react';
+import styled from 'styled-components';
+
+const KarakterCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  font-size: 1.2rem;
+  margin: 10px;
+  text-align: center;
+  color: cyan;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Collapse = styled.div`
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin: 10px;
+  color: black;
+  max-height: ${props => props.open ? '500px' : '0'};
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+  width: 100%;
+`;
+
+const CardBody = styled.div`
+  padding: 10px;
+  width: 30rem;
+
+`;
+
+function Karakter({data}) {
+  const [openIndex, setOpenIndex] = React.useState(null);
+  
+  // console.log("karakterler: ", data)
+
+  const handleClick = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  }
+
+  return (
+    <KarakterCard>
+      {Array.isArray(data) &&
+        data.map((karakter, index) => (
+          <div key={index}>
+            <Button onClick={() => handleClick(index)}>
+              {karakter.name}
+            </Button>
+            <Collapse open={openIndex === index}>
+              <CardBody>
+                <p>Height: {karakter.height}</p>
+                <p>Mass: {karakter.mass}</p>
+                <p>Hair color: {karakter.hair_color}</p>
+                <p>Skin color: {karakter.skin_color}</p>
+                <p>Eye color: {karakter.eye_color}</p>
+                <p>Birth year: {karakter.birth_year}</p>
+                <p>Gender: {karakter.gender}</p>
+                <p>Films: {karakter.films}</p>
+              </CardBody>
+            </Collapse>
+          </div>
+        ))}
+    </KarakterCard>
+  );
+};
+
+export default Karakter;
